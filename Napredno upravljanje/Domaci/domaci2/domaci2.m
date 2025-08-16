495c3eaccbf1694d04d5d079faadfb2c05c2b8ba
usernamenenad <radovic.nenad158@gmail.com>
2025-08-16 10:57:27 +0200

    Modified Domaci2

--- Napredno upravljanje/Domaci/domaci2/domaci2.m
+++ Napredno upravljanje/Domaci/domaci2/domaci2.m
@@ -8,19 +8,19 @@ n = rank(ctrb(A, B));
 K = acker(A, B, [-3, -3]);
 x0 = [-2; 2];
 
-%% Promjenljive stanja (prvo simulirati model2.slx)
+%% Promjenljive stanja (prvo simulirati model2_2020.slx)
 t = out.tout;
-x1 = out.x.Data(:, 1);
-x2 = out.x.Data(:, 2);
+x1 = out.yout{1}.Values.Data(:, 1);
+x2 = out.yout{1}.Values.Data(:, 2);
 
-plot(t, x1, LineWidth=1.5);
+plot(t, x1, 'LineWidth', 1.5);
 hold on;
 grid on;
-plot(t, x2, LineWidth=1.5);
-xlabel('$t$', Interpreter='Latex');
+plot(t, x2, 'LineWidth', 1.5);
+xlabel('$t$', 'Interpreter', 'Latex');
 legend('$x_{1}(t)$', '$x_{2}(t)$',...
-       Interpreter = 'Latex',...
-       FontSize = 12)
+       'Interpreter', 'Latex',...
+       'FontSize', 12)
 
 %% Računanje kr
 s = tf("s");
